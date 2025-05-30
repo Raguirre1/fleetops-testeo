757f017b7ad214abc6a99df4b58f86a637189783
Raguirre1 <140976384+Raguirre1@users.noreply.github.com>
2025-05-30 12:44:32 +0000

    Fix CORS preflight manual

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const SECRET_KEY = process.env.JWT_SECRET || "mi_clave_secreta";
 
 // ==== CORS CONFIGURADO PARA DESARROLLO ====
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // 🔁 puedes reemplazar "*" por el dominio exacto si quieres limitarlo
+  res.header("Access-Control-Allow-Origin", "fleetops-production.up.railway.app"); // 🔁 puedes reemplazar "*" por el dominio exacto si quieres limitarlo
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
